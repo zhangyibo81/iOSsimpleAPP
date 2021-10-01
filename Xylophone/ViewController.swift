f130b357b1c45dc8988f682bd189c8754d0da257
张弈搏 <zhangyibo@zhangyibodeMacBook-Air.local>
2021-10-01 15:00:52 -0700

    musicp

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,34 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+    @IBAction func keyPressed(_ sender: UIButton){
+        playSound(soundMusic: sender.currentTitle!)
+        //Reduces the sender's (the button that got pressed) opacity to half.
+        print("start")
+        sender.alpha = 0.5
 
+          //Code should execute after 0.2 second delay.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+          //Bring's sender's opacity back up to fully opaque.
+            print("end")
+            sender.alpha = 1.0
+        }
+    }
     
-
+    func playSound(soundMusic: String) {
+        let url = Bundle.main.url(forResource: soundMusic, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
-
